c2ec151a8fc44802a6e12db31a0b67e2ff8bb517
Pranay Gonipalli <pranaygonipalli1303@gmail.com>
2024-02-16 11:07:55 +0530

    Allfiles

--- obj/Debug/net6.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net6.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2426b8f16fcf9eb725f51e20a65d22d2b1d9fe29")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
