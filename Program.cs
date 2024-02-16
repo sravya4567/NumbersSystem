2426b8f16fcf9eb725f51e20a65d22d2b1d9fe29
Pranay Gonipalli <pranaygonipalli1303@gmail.com>
2024-02-16 09:43:27 +0530

    AllFiles

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+Console.WriteLine("Welcome to Number System");
+Console.WriteLine("*** Even Numbers***");
+NumberSystem.EvenNum();
+Console.WriteLine("***Odd Numbers");
+NumberSystem.OddNum();
